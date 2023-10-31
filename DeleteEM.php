d4d4b6431761779a9bf07e31cbf8d8b960ab5fbf
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-10-31 13:13:36 -0400

    Added Delete sql entry.

--- DeleteEM.php
+++ DeleteEM.php
@@ -4,28 +4,35 @@ $username = "homebasedb";
 $password = "homebasedb";
 $db = "homebasedb";
 $connection = mysqli_connect($servername, $username, $password, $db);
-	if (mysqli_connect_errno()){
-    		echo "Failed to connect to the database" . mysqli_connect_error();
-    		exit();
-	} else {
-    		echo "Successfully connected to the database! <br>";
-	}
-	if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete"])) {
-		$material_id = $_POST["materialid"];
-	}
+if (mysqli_connect_errno()){
+    echo "Failed to connect to the database" . mysqli_connect_error();
+    exit();
+} else {
+    echo "Successfully connected to the database! <br>";
+}
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete"])) {
+    $material_id = $_POST["materialid"];
+    $sql = "DELETE FROM homebasedb WHERE materialid = '$material_id'";
+    if ($connection->query($sql) === TRUE){
+        echo "Educational Material has been deleted.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connection->error;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset = "utf-8">
-	<title>Delete Educational Material</title>
+    <meta charset = "utf-8">
+    <title>Delete Educational Material</title>
 </head>
 <body>
-	<form method="POST" action="">
-		<input type="hidden" name="operation" value="delete">
-		<label for="materialid">EM ID:</label>
-		<input type="text" id="materialid" name="materialid" value="<?php echo $material_id; ?>"<br>
-		<input type="submit" name="delete" value="Delete">	
-	</form>
+<form method="POST" action="">
+    <input type="hidden" name="operation" value="delete">
+    <label for="materialid">EM ID:</label>
+    <input type="text" id="materialid" name="materialid" value="<?php echo $material_id; ?>"<br>
+    <input type="submit" name="delete" value="Delete">
+</form>
 </body>
 </html>
+
