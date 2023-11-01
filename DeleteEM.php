6dd94dce834978a7d20b31d224d4574d6d11cb5b
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-11-01 09:46:27 -0400

    Update DeleteEM.php
    Added that it checks if the material_id exists and if it doesn't then it sends an error message invalid educational material id. Also, fixed some other errors that I had in it. It should be ready for testing.

--- DeleteEM.php
+++ DeleteEM.php
@@ -12,11 +12,17 @@ if (mysqli_connect_errno()){
 }
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete"])) {
     $material_id = $_POST["materialid"];
-    $sql = "DELETE FROM homebasedb WHERE materialid = '$material_id'";
+    $sql = "SELECT FROM emdb WHERE materialid = '$material_id'";
+    $results = $connection->query($sql);
+    if($results->num_rows > 0){
+        $sql = "DELETE FROM emdb WHERE materialid = '$material_id'";
     if ($connection->query($sql) === TRUE){
         echo "Educational Material has been deleted.";
     } else {
         echo "Error: " . $sql . "<br>" . $connection->error;
+    }
+} else {
+        echo "Invalid educational material ID. Please enter a valid ID.";
     }
 }
 ?>
@@ -27,12 +33,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete"])) {
     <title>Delete Educational Material</title>
 </head>
 <body>
+<h1>Delete Educational Material</h1>
 <form method="POST" action="">
     <input type="hidden" name="operation" value="delete">
     <label for="materialid">EM ID:</label>
     <input type="text" id="materialid" name="materialid" value="<?php echo $material_id; ?>"<br>
     <input type="submit" name="delete" value="Delete">
 </form>
+<script>
+    function confirmDelete(){
+        var confirmed = confirm("Are you sure that you want to delete this educational material?");
+        return confirmed;
+    }
+</script>
 </body>
 </html>
 
