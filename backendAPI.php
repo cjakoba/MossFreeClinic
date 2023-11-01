489e3bb0192611643ccc63a14c5908c07d998ecb
Shane Alexander <salexand@mail.umw.edu>
2023-11-01 00:18:51 -0400

    added getEM() function to backendAPI.php which runs SQL query for search EM and calls encode to store in json-encoded data structure

--- backendAPI.php
+++ backendAPI.php
@@ -9,4 +9,21 @@ function encode($name, $date, $author, $fileType, $description, $filePath){
     $data->filepath = $filePath;
     echo json_encode($data);
 }
+
+function getEM($materialid, $conn){
+    $stmt = $conn->prepare("SELECT * FROM emdb WHERE materialid=?");
+    $stmt->bind_param("i", $materialid)
+    $results = $stmt->execute();
+    while($row = $results->fetch_assoc()) {
+        encode($row["name"], $row["upload_date"], $row["uploaded_by"], $row["file_type"], $row["description"], $row["upload_file_path"])
+    }
+
+
+
+}
+
+
+
+
+
 ?>
