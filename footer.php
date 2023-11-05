7041a3bd385377058bcad66f942d51db9bf231c2
cjakoba <cajakob@icloud.com>
2023-11-05 13:20:18 -0500

    Added styling for creation of EM

--- footer.php
+++ footer.php
@@ -1,70 +1,100 @@
-<?php
-    include('session.php');
-?>
-
-<?php
-/*
- * Copyright 2023 by Joon Yi. 
- * This program is part of CVHR Project, which is free software.  It comes with 
- * absolutely no warranty. You can redistribute and/or modify it under the terms 
- * of the GNU General Public License as published by the Free Software Foundation
- * (see <http://www.gnu.org/licenses/ for more information).
- * 
- */
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        html,
-        body {
-            height: 100%;
+        .footer-background {
+            background-color: #05547f; /* Adjust the color to match your design */
+            color: white;
+            padding: 40px 0; /* Padding as needed */
         }
-        .wrapper {
-            min-height: 100%;
-            margin-bottom: -50px; /* equal to footer height */
+
+        .footer-link {
+            color: white;
+            text-decoration: none;
+            margin-bottom: 10px;
+            display: block; /* Makes it easier to click on */
         }
-        .footer,
-        .push {
-            height: 50px;
+
+        .footer-link:hover {
+            text-decoration: underline;
         }
-        .footer {
-            background-color: #f8f9fa;
-            border-top: 1px solid #dee2e6;
-            padding: 10px 0;
-            width: 100%;
-            text-align: center;
+
+        .footer-heading {
+            color: #aad1ff; /* Lighter shade for the heading */
+            margin-bottom: 15px;
+            border-bottom: 1px solid #aad1ff; /* Underline effect */
+            padding-bottom: 5px;
         }
-        .footer p {
-            margin: 0;
-            font-size: 14px;
-            color: #6c757d;
+
+        .footer-logo {
+            margin-bottom: 20px;
         }
-        @media (max-width: 768px) {
-            .wrapper {
-                margin-bottom: -100px; /* increase negative margin to account for smaller footer height */
-            }
-            .footer,
-            .push {
-                height: 100px; /* increase height of footer and push elements */
-            }
-            .footer p {
-                font-size: 12px; /* decrease font size of footer text */
-            }
+
+        .social-icon {
+            color: white;
+            margin-right: 10px;
+        }
+
+        .footer-logo img {
+            max-height: 100px; /* Adjust based on your logo's size */
+        }
+
+        .footer-address {
+            margin-top: 20px; /* Spacing from the bottom elements */
+            font-size: 0.85em;
+        }
+
+        .footer-badge {
+            max-width: 150px; /* Adjust the size of the badge */
+            margin: 20px 0;
         }
     </style>
 </head>
 <body>
-    <div class="wrapper">
-        <!-- Begin content here -->
+
+<!-- Footer -->
+<footer class="footer-background">
+    <div class="container">
+        <div class="row">
+            <!-- Footer column 1 -->
+            <div class="col-md">
+                <h5 class="footer-heading">Get Treatment</h5>
+                <a href="#" class="footer-link">Patient Eligibility</a>
+                <a href="#" class="footer-link">Clinic Services</a>
+            </div>
+
+            <!-- Footer column 2 -->
+            <div class="col-md">
+                <h5 class="footer-heading">Get Involved</h5>
+                <a href="#" class="footer-link">Get Involved</a>
+                <a href="#" class="footer-link">Donate</a>
+            </div>
+
+            <!-- Footer column 3 -->
+            <!-- Add other columns as needed -->
+
+            <!-- Footer column with logo -->
+            <div class="col-md text-center footer-logo">
+                <img src="img/logo-white.png" alt="Clinic Logo">
+                <div>
+                    <img src="img/reward.png" alt="NAFC 2020 Standards Gold" class="footer-badge">
+                </div>
+            </div>
+            <!-- Add other columns as needed -->
+        </div>
+        <div class="row footer-address">
+            <div class="col-md-12 text-center">
+                ©2023 Lloyd F. Moss Free Clinic | 1301 Sam Perry Blvd. Fredericksburg, VA 22401 | (540) 582-1061
+                <br>
+                website created by The Childress Agency
+            </div>
+        </div>
     </div>
-    <div class="push"></div>
-    <!-- Begin Footer -->
-    <footer class="footer">
-        <p>&copy; <?php echo date('Y'); ?> Lloyd F. Moss Free Clinic. All rights reserved.</p>
-    </footer>
-    <!-- End Footer -->
+</footer>
 </body>
 </html>
+
