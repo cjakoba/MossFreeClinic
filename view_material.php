9cdb6ceb1a008e6387585b5231f104187ce7de03
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-11-09 13:25:42 -0500

    Update view_material.php
    I added the front end of the rating to the view educational material. I will continue it to make it function correctly.

--- view_material.php
+++ view_material.php
@@ -27,6 +27,23 @@
             <!-- Main body content -->
             <div class="col-lg-11">
                 <div id="content"></div>
+                <form method="post">
+                    <h2>Rate this educational material:</h2>
+                    <span>class="star" onclick="rate(1)">&#9733;</span>
+                    <span>class="star" onclick="rate(2)">&#9733;</span>
+                    <span>class="star" onclick="rate(3)">&#9733;</span>
+                    <span>class="star" onclick="rate(4)">&#9733;</span>
+                    <span>class="star" onclick="rate(5)">&#9733;</span>
+                    <button type="submit" name="Submit">Submit</button>
+                </form>
+                <?php
+                $message = "Thanks for the review.";
+                if(isset($_POST["Submit"])){
+                    echo $message;
+                } else {
+                    echo "You haven't submitted a review yet.";
+                }
+                ?>
             </div>
         </div>
     </div>
