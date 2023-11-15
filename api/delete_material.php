ede0f0a559b084a489d746b70f91d7a72dc54629
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-11-15 16:42:02 -0500

    Update and rename DeleteEM.php to delete_material.php
    This is the backend for delete. Also, I kept the function in there for now, but I am going to put the function into the file where the delete button is, so it shows the confirmation message to the actor.

--- api/delete_material.php
+++ api/delete_material.php
@@ -11,41 +11,21 @@ if (mysqli_connect_errno()){
     echo "Successfully connected to the database! <br>";
 }
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete"])) {
-    $material_id = $_POST["materialid"];
-    $sql = "SELECT FROM emdb WHERE materialid = '$material_id'";
+    $post_id = $_POST["post_id"];
+    $sql = "SELECT FROM em_posts WHERE post_id = '$post_id'";
     $results = $connection->query($sql);
     if($results->num_rows > 0){
-        $sql = "DELETE FROM emdb WHERE materialid = '$material_id'";
+        $sql = "DELETE FROM em_posts WHERE post_id = '$post_id'";
     if ($connection->query($sql) === TRUE){
         echo "Educational Material has been deleted.";
     } else {
         echo "Error: " . $sql . "<br>" . $connection->error;
     }
-} else {
-        echo "Invalid educational material ID. Please enter a valid ID.";
-    }
-}
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset = "utf-8">
-    <title>Delete Educational Material</title>
-</head>
-<body>
-<h1>Delete Educational Material</h1>
-<form method="POST" action="">
-    <input type="hidden" name="operation" value="delete">
-    <label for="materialid">EM ID:</label>
-    <input type="text" id="materialid" name="materialid" value="<?php echo $material_id; ?>"<br>
-    <input type="submit" name="delete" value="Delete">
-</form>
 <script>
     function confirmDelete(){
         var confirmed = confirm("Are you sure that you want to delete this educational material?");
         return confirmed;
     }
 </script>
-</body>
-</html>
 
