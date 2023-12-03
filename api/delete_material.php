7c6f58658573de8120c9c1ab7cfec55b95ca81b5
brendenhigh <117188061+brendenhigh@users.noreply.github.com>
2023-12-03 17:40:28 -0500

    Update delete_material.php
    This adds better look to the page.

--- api/delete_material.php
+++ api/delete_material.php
@@ -10,89 +10,81 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["post_id"])) {
-    $post_id = $_GET["post_id"];
-    echo "<h2>Delete Educational Material</h2>";
-    echo "<form id='deleteForm' method='POST' action=''>";
-    echo "<input type='hidden' name='operation' value='delete'>";
-    echo "<label for='post_id'>Post ID:</label>";
-    echo "<input type='text' id='post_id' name='post_id' readonly><br>";
-    echo "<button type='button' onclick='showCustomConfirmation()'>Delete</button>";
-    echo "</form>";
+include 'layouts/header-editorjs.php';
+include 'layouts/navbar.php';
+?>
 
-    echo "<script>
-            document.getElementById('post_id').value = '$post_id';
+<div class='container'>
+    <h2>Delete Educational Material</h2>
+    <form id='deleteForm' method='POST' action=''>
+        <input type='hidden' name='operation' value='delete'>
+        <label for='post_id'>Post ID:</label>
+        <input type='text' id='post_id' name='post_id' readonly><br>
+        <button type='button' onclick='showCustomConfirmation()'>Delete</button>
+    </form>
+</div>
 
-            function showCustomConfirmation() {
-                var confirmation = document.getElementById('customConfirmation');
-                confirmation.style.display = 'block';
-            }
+<div id="customConfirmation" style="display: none;">
+    Are you sure you want to delete this?
+    <button onclick="deleteConfirmed()">Yes</button>
+    <button onclick="deleteCanceled()">No</button>
+</div>
 
-            function deleteConfirmed() {
-                // Continue with the deletion of the post
-                var postForm = document.getElementById('deleteForm');
-                postForm.submit();
-            }
+<?php include 'layouts/footer.php'; ?>
 
-            function deleteCanceled() {
-                var confirmation = document.getElementById('customConfirmation');
-                confirmation.style.display = 'none';
-            }
-          </script>";
-}
+<script>
+    document.getElementById('post_id').value = '<?php echo isset($_GET["post_id"]) ? $_GET["post_id"] : ""; ?>';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["post_id"])) {
+    function showCustomConfirmation() {
+        var confirmation = document.getElementById('customConfirmation');
+        if (confirmation) {
+            confirmation.style.display = 'block';
+        } else {
+            console.error('Custom confirmation element not found');
+        }
+    }
+
+    function deleteConfirmed() {
+        var postForm = document.getElementById('deleteForm');
+        if (postForm) {
+            postForm.submit();
+        } else {
+            console.error('Delete form not found');
+        }
+    }
+
+    function deleteCanceled() {
+        var confirmation = document.getElementById('customConfirmation');
+        if (confirmation) {
+            confirmation.style.display = 'none';
+        } else {
+            console.error('Custom confirmation element not found');
+        }
+    }
+</script>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["operation"]) && $_POST["operation"] == "delete") {
     $post_id = $_POST["post_id"];
 
     $deleteRatingsQuery = "DELETE FROM ratingdb WHERE em_post_id = '$post_id'";
     if ($connection->query($deleteRatingsQuery) === TRUE) {
-        // Ratings deleted, now delete the post
         $deletePostQuery = "DELETE FROM em_posts WHERE post_id = '$post_id'";
         if ($connection->query($deletePostQuery) === TRUE) {
-            echo 'Educational Material and Ratings successfully deleted.';
-            // Redirect the user back to the previous page
-            echo '<script>window.location.href = "../views/list_em_posts.php";</script>';
+            echo '<script>window.location.href = "list_em_posts.php";</script>';
+            exit();
         } else {
-            echo 'Error deleting post: ' . $connection->error;
-            echo '<script>window.history.back();</script>';
+            echo 'console.error("Error deleting post: ' . $connection->error . '");';
+            exit();
         }
     } else {
-        echo 'Error deleting ratings: ' . $connection->error;
-        echo '<script>window.history.back();</script>';
+        echo 'console.error("Error deleting ratings: ' . $connection->error . '");';
+        exit();
     }
+
+    $connection->close();
 }
-
-$connection->close();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Delete Educational Material</title>
-    <style>
-        #customConfirmation {
-            display: none;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: white;
-            padding: 20px;
-            border: 1px solid #ccc;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            z-index: 1000;
-        }
 
-        #customConfirmation button {
-            margin-right: 10px;
-        }
-    </style>
-</head>
-<body>
-    <div id="customConfirmation">
-        <p>Are you sure you want to delete this educational material?</p>
-        <button onclick="deleteConfirmed()">Yes</button>
-        <button onclick="deleteCanceled()">No</button>
-    </div>
 </body>
 </html>
